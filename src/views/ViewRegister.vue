26f41fdd4c0388c7a7d8cbe23b9aaae04b7cf9d4
Daein12 <luster6951@gmailc.om>
2018-11-13 18:52:21 +0900

    2nd by KPS

--- src/views/ViewRegister.vue
+++ src/views/ViewRegister.vue
@@ -1,15 +1,17 @@
 <template>
     <div>
-
+        <SignUp/>
     </div>
 </template>
 
 <script>
+    import SignUp from "../components/SignUp";
     export default {
-        name: "ViewRegister"
+        name: "ViewRegister",
+        components: {SignUp}
     }
 </script>
-
+import SignUp from '../components/SignUp.vue'
 <style scoped>
 
 </style>
